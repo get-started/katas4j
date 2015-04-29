bb1b80828b04ca39fbc8cde6168b763b42df426e
xiaoliang <32414154@qq.com>
2015-04-29 12:23:41 +0800

    # say hello to Mr if somebody is man

---
+++ src/test/java/test/katas/TeaPartyTest.java
@@ -0,0 +1,16 @@
+package test.katas;
+
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Created by L.x on 15-4-29.
+ */
+public class TeaPartyTest {
+    @Test
+    public void sayMrXXXToAMan() throws Exception {
+        assertThat(TeaParty.welcome("Orwell"),equalTo("Hello Mr. Orwell"));
+    }
+}
