5626d25e7a8d087f36417dadf1305a93afe9d22e
xiaoliang <32414154@qq.com>
2015-04-29 11:48:36 +0800

    # FuzzBuzz.of(1)=="1"

---
+++ src/test/java/test/katas/FuzzBuzzTest.java
@@ -0,0 +1,16 @@
+package test.katas;
+
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Created by L.x on 15-4-29.
+ */
+public class FuzzBuzzTest {
+    @Test
+    public void normal() throws Exception {
+        assertThat(FuzzBuzz.of(1),equalTo("1"));
+    }
+}
